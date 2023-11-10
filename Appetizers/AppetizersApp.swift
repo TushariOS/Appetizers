cfb93a04190c0279915591bc31e0f02edf9dd11b
JTushar <tushar@heady.io>
2023-11-10 17:11:54 +0530

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Tushar Jaunjalkar on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
